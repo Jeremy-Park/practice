1294ef76f0f4e18f9a1099b383950d550535c74b
Jeremy-Park <32861944+Jeremy-Park@users.noreply.github.com>
2021-11-10 17:25:53 -0500

    routes
    -now uses routes

--- game lobby/client/src/App.jsx
+++ game lobby/client/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
-import Lobby from './components/Lobby';
+import "./App.css";
+import Lobby from "./components/Lobby";
+import Player from "./components/Player";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App">
-      <Lobby />
+    <div className="app">
+      {/* persistent elements like navbar live outside of router
+      children of router must be route or fragment
+      v6 doesnt use =component or switch */}
+      <Router>
+        <Routes>
+          {/* There is only one page so / */}
+          <Route exact path="/" element={<Lobby />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
